55933979c3389031f3c65214db685acc03748f3e
MatteoAgu01 <matteoaguiari012@gmail.com>
2023-05-31 17:21:13 +0200

    fix controller

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -40,7 +40,7 @@ class ComicController extends Controller
 	public function store(Request $request)
 	{
 		$form_data = $this->validation($request->all());
-		$form_data = $request->all();
+
 		$newComic = new Comic();
 		$newComic->fill($form_data);
 		$newComic->save();
@@ -79,7 +79,6 @@ class ComicController extends Controller
 	public function update(Request $request, Comic $comic)
 	{
 		$form_data = $this->validation($request->all());
-		$form_data = $request->all();
 		$comic->update($form_data);
 		return redirect()->route('comics.index', $comic->id);
 	}
@@ -117,14 +116,14 @@ class ComicController extends Controller
 				'title.required' => 'Il titolo è obbligatorio',
 				'title.max' => 'Il titolo non deve superare 255 caratteri',
 				'title.min' => 'Il titolo deve contenere almeno tre caratteri',
-				'description.min'=>'Il minimo dei caratteri accettati è 50',
-				'description.max'=>'Il massimo dei caratteri accettati è 255',
-				'description.required'=>'questo campo è obbligatorio',
-				'thumb.required'=> 'Devi inserire l\' url di un immagine presa da google',
-				'price.required'=> ' controlla di aver messo il simbolo "$" prima del prezzo',
-				'series.required'=> 'questo campo è obbligatorio',
-				'type.required'=> 'questo campo è obbligatorio',
-				'sale_date.required'=> 'questo campo è obbligatorio',
+				'description.min' => 'Il minimo dei caratteri accettati è 50',
+				'description.max' => 'Il massimo dei caratteri accettati è 255',
+				'description.required' => 'questo campo è obbligatorio',
+				'thumb.required' => 'Devi inserire l\' url di un immagine presa da google',
+				'price.required' => ' controlla di aver messo il simbolo "$" prima del prezzo',
+				'series.required' => 'questo campo è obbligatorio',
+				'type.required' => 'questo campo è obbligatorio',
+				'sale_date.required' => 'questo campo è obbligatorio',
 
 			]
 		)->validate();
